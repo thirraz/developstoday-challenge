1a3ada1e20a1bf13cbcc20927d6faa22ec59b721
Thiago Souza <thiagofernandosouza20@gmail.com>
2024-09-11 10:43:07 -0300

    update: removin scrollbar transition

--- src/components/GridContainer.tsx
+++ src/components/GridContainer.tsx
@@ -11,11 +11,6 @@ function GridContainer({ children }: Props) {
 	const { scrollYProgress } = useScroll({
 		container: containerRef
 	})
-	const scaleY = useSpring(scrollYProgress, {
-		stiffness: 100,
-		damping: 30,
-		restDelta: 0.001
-	})
 
 	return (
 		<div
@@ -23,8 +18,8 @@ function GridContainer({ children }: Props) {
 			className="relative max-h-[calc(100dvh-4rem)] grid col-2 border-b rounded-md border-slate-200 max-w-[800px] mx-auto overflow-y-scroll divide-y divide-slate-700"
 		>
 			<motion.div
-				className="fixed top-0 bottom-0 right-0 w-4 bg-slate-400 z-50 origin-top rounded-b-xl "
-				style={{ scaleY }}
+				className="fixed top-0 bottom-0 right-0 w-4 bg-slate-400 z-50 origin-top "
+				style={{ scaleY: scrollYProgress }}
 			/>
 			<GridHeading />
 			{children}
