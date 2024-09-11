1095b4a1bdb0a099606078e926457d2bab721354
Thiago Souza <thiagofernandosouza20@gmail.com>
2024-09-11 11:29:20 -0300

    update: renaning getCarMakes() function and removing .env file

--- src/lib/getCarMakes.ts
+++ src/lib/getCarMakes.ts
@@ -3,7 +3,7 @@ export async function getCarMakes() {
 		"https://vpic.nhtsa.dot.gov/api/vehicles/GetMakesForVehicleType/car?format=json"
 	)
 
-	const data = (await res).json()
+	const data = await res.json()
 
 	return data
 }
